b0af9ad4aa5708a1df8ef0a887968f6aab8416a3
SeaWal <sean.walshe@tutanota.com>
2026-09-10 22:09:44 +0100

    modern style types, may use in cx

---
+++ include/cx_types.h
@@ -0,0 +1,31 @@
+#ifndef CX_TYPES_H
+#define CX_TYPES_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+typedef uint8_t   u8;
+typedef uint16_t  u16;
+typedef uint32_t  u32;
+typedef uint64_t  u64;
+
+typedef int8_t    i8;
+typedef int16_t   i16;
+typedef int32_t   i32;
+typedef int64_t   i64;
+
+typedef float     f32;
+typedef double    f64;
+
+typedef bool      bool;
+
+typedef size_t    size_t;
+typedef size_t    usize;
+
+typedef ptrdiff_t ptrdiff_t;
+typedef ptrdiff_t isize;
+
+typedef uint8_t   byte;
+
+#endif // CX_TYPES_H

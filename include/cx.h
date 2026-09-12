ea9bb66443d762dfc9177a46d0ce36124f8281c8
SeaWal <sean.walshe@tutanota.com>
2026-09-12 22:05:28 +0100

    add cpp guards to global header

--- include/cx.h
+++ include/cx.h
@@ -1,7 +1,15 @@
 #ifndef CX_INCLUDE_H
 #define CX_INCLUDE_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "cx_core.h"
 #include "cx_types.h"
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // CX_INCLUDE_H

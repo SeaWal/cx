716b891e3f971ee3a971f0aedb9ba293d9f29c2f
SeaWal <sean.walshe@tutanota.com>
2026-09-16 21:45:41 +0100

    added interface for arena allocator

--- include/cx_allocator.h
+++ include/cx_allocator.h
@@ -6,12 +6,13 @@ extern "C" {
 #endif
     
 #include "cx_core.h"
+#include "cx_types.h"
 
 /*
-* Allocator interface for custom memory management.
-* Provides hooks for allocation/deallocation of memory with
-* alignment support.
-*/
+ * Allocator interface for custom memory management.
+ * Provides hooks for allocation/deallocation of memory with
+ * alignment support.
+ */
 typedef struct cx_allocator cx_allocator;
 
 struct cx_allocator {
@@ -27,8 +28,8 @@ struct cx_allocator {
     /*
      * Deallocates a previously allocated block of memory
      * 
-     * @param ptr  Pointer to the memory block.
-     * @param size Size of the block (for debugging/tracking).
+     * @param ptr   Pointer to the memory block.
+     * @param size  Size of the block (for debugging/tracking).
      * @param align Alignment of the block (for debugging/tracking).
      */
     void  (*dealloc)(void* ptr, size_t size, size_t align);
@@ -38,10 +39,65 @@ struct cx_allocator {
  * Creates the default system allocator.
  * For simple, general-purpose use-cases.
  * 
- * @return  Allocator wrapper around malloc/free.
- * 
+ * @return Allocator wrapper around malloc/free. 
  */
 CX_API cx_allocator cx_general_allocator(void);
+
+
+/*
+ * Arena allocator.
+ *
+ * Allocates memory linearly from a buffer.
+ * Individual allocations can't be deallocated.
+ * Memory is reclaimed when the arena is destroyed.
+ */
+typedef struct cx_arena cx_arena;
+
+struct cx_arena {
+    cx_allocator allocator;
+
+    u8* buffer;
+    size_t capacity;
+    size_t pos;
+};
+
+/*
+ * Initialises an arena with the specified capacity.
+ * 
+ * The arena allocates its buffer from the heap.
+ * 
+ * @param arena Arena to initialise.
+ * @param capacity Size of the buffer in bytes.
+ * @return true on success, false on failure to allocate.
+ */
+CX_API bool cx_arena_init(cx_arena* arena, size_t capacity);
+
+/*
+ * Returns the allocator interface for the given arena.
+ * 
+ * @param arena The arena to create an allocator for.
+ * @return Allocator backed by the arena.
+ */
+CX_API cx_allocator cx_arena_allocator(cx_arena* arena);
+
+/*
+ * Resets the arena.
+ * 
+ * All memory previously allocated from the arena becomes invalid.
+ * 
+ * @param arena The arena to reset.
+ */
+CX_API void cx_arena_reset(cx_arena* arena);
+
+/*
+ * Destroys the arena.
+ *
+ * Releases the underlying buffer and invalidates all allocations
+ * made from the arena.
+ * 
+ * @param arena The arena to destroy.
+ */
+CX_API void cx_arena_destroy(cx_arena* arena);
 
 #ifdef __cplusplus
 }

105fb74f007e8e2c31f67464323465863b13c383
SeaWal <sean.walshe@tutanota.com>
2026-09-17 17:58:06 +0100

    implemented API for arena allocator

--- src/cx_allocator.c
+++ src/cx_allocator.c
@@ -23,3 +23,108 @@ CX_API cx_allocator cx_general_allocator(void) {
         .ctx = NULL,
     };
 }
+
+static bool is_power_of_two(size_t value) {
+    return value != 0 && (value & (value - 1)) == 0;
+}
+
+static bool align_forward(size_t value, size_t align, size_t* result) {
+    if(!is_power_of_two(align)) {
+        return false;
+    }
+
+    size_t mask = align - 1;
+
+    if(value > SIZE_MAX - mask) {
+        return false;
+    }
+
+    *result = (value + mask) & ~mask;
+    return true;
+}
+
+static void* cx_arena_alloc(void* ctx, size_t size, size_t align) {
+    cx_arena* arena = ctx;
+    if(arena == NULL || size == 0) {
+        return NULL;
+    }
+
+    size_t position;
+    if(!align_forward(arena->pos, align, &position)) {
+        return NULL;
+    }
+
+    if(position > arena->capacity) {
+        return NULL;
+    }
+
+    if(size > arena->capacity - position) {
+        return NULL;
+    }
+
+    void* ptr = arena->buffer + position;
+
+    arena->pos = position + size;
+
+    return ptr;
+}
+
+// dealloc not defined for individual deallocations with arena
+static void cx_arena_dealloc(void* ctx, void* ptr, size_t size, size_t align) {
+    CX_UNUSED(ctx);
+    CX_UNUSED(ptr);
+    CX_UNUSED(size);
+    CX_UNUSED(align);
+}
+
+CX_API bool cx_arena_init(cx_arena* arena, size_t capacity) {
+    if(arena == NULL || capacity == 0) {
+        return false;
+    }
+
+    u8* buffer = malloc(capacity);
+    if(buffer == NULL) {
+        return false;
+    }
+
+    arena->buffer = buffer;
+    arena->capacity = capacity;
+    arena->pos = 0;
+
+    arena->allocator = (cx_allocator) {
+        .alloc = cx_arena_alloc,
+        .dealloc = cx_arena_dealloc,
+        .ctx = arena,
+    };
+
+    return true;
+}
+
+CX_API cx_allocator cx_arena_allocator(cx_arena* arena) {
+    if(arena == NULL) {
+        return (cx_allocator) { 0 };
+    }
+
+    return arena->allocator;
+}
+
+CX_API void cx_arena_reset(cx_arena* arena) {
+    if(arena == NULL) {
+        return;
+    }
+
+    arena->pos = 0;
+}
+
+
+CX_API void cx_arena_destroy(cx_arena* arena) {
+    if(arena == NULL) {
+        return;
+    }
+
+    free(arena->buffer);
+    arena->buffer = NULL;
+    arena->capacity = 0;
+    arena->pos = 0;
+    arena->allocator = (cx_allocator) { 0 };
+}
